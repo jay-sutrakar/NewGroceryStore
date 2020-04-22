8cbcbe140ee05f3fe8de174a63530892c431a82b
jay-sutrakar <53367492+jay-sutrakar@users.noreply.github.com>
2020-04-22 22:46:14 +0530

    Register user Login user Authentication on fireStore

--- app/src/main/java/com/example/grocerystore/RegisterActivity.java
+++ app/src/main/java/com/example/grocerystore/RegisterActivity.java
@@ -1,33 +1,49 @@
 package com.example.grocerystore;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
     private Button createAccountButton;
     private EditText usernameInput, passwordInput, phoneInput, emailInput;
     private ProgressDialog loadingBar;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore db=FirebaseFirestore.getInstance();
+    private CollectionReference collectionReference = db.collection("Users");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
+        //Initialization of firebaseauth
+        firebaseAuth = FirebaseAuth.getInstance();
+        Log.d("FirebaseAuth instance ", "onCreate: "+firebaseAuth);
         //Initializing
-        createAccountButton = (Button) findViewById(R.id.register_button);
-        usernameInput = (EditText) findViewById(R.id.register_username_input);
-        phoneInput = (EditText) findViewById(R.id.register_phone_input);
-        passwordInput = (EditText) findViewById(R.id.register_password_input);
-        emailInput = (EditText) findViewById(R.id.register_email_input);
+        createAccountButton = findViewById(R.id.register_button);
+        usernameInput =  findViewById(R.id.register_username_input);
+        phoneInput =  findViewById(R.id.register_phone_input);
+        passwordInput =  findViewById(R.id.register_password_input);
+        emailInput =  findViewById(R.id.register_email_input);
         loadingBar = new ProgressDialog(this);
 
         //On click create account
@@ -66,6 +82,30 @@ public class RegisterActivity extends AppCompatActivity {
 
     //write your code here for adding customer into database
     private void validateUser(final String username, final String phone, final String password, final String email) {
-
+        firebaseAuth.createUserWithEmailAndPassword(email,password)
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        Map<String,String> user = new HashMap<>();
+                        user.put("userId",firebaseAuth.getUid());
+                        user.put("username",username);
+                        // Here we are adding the details of user and its id in firestore cloud database
+                        collectionReference.add(user)
+                                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                    @Override
+                                    public void onSuccess(DocumentReference documentReference) {
+                                        loadingBar.dismiss();
+                                        Toast.makeText(RegisterActivity.this, "Account created", Toast.LENGTH_SHORT)
+                                                .show();
+                                    }
+                                });
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("Error in sign in ", "onFailure: "+e.getMessage());
+                    }
+                });
     }
 }
